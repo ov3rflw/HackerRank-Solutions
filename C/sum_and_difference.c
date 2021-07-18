83e9e41f155268830f60a0c6c417910073ef3fee
Pierre Loti <68467919+one6away@users.noreply.github.com>
2021-07-19 01:02:23 +0200

    Create sum_and_difference.c

---
+++ C/sum_and_difference.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	  int nb1,nb2;
+    float nb3,nb4;
+    
+    scanf("%d %d %f %f",&nb1, &nb2, &nb3, &nb4);
+    
+    printf("%d %d", nb1+nb2, nb1-nb2);
+    printf("\n%.1f %.1f", nb3+nb4, nb3-nb4);
+    
+    return 0;
+}
